fa7e9098d8a2466532328bc588297e28c8c22ba5
charmant27 <charmantogatera@gmail.com>
2022-12-15 19:51:25 -0800

    imported new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { TouristicPlaceModule } from './touristic-place/touristic-place.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AdminModule, AuthModule, TouristicPlaceModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AdminModule,
+    AuthModule,
+    TouristicPlaceModule,
+    UserModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
