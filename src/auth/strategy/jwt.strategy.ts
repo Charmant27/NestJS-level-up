acb953487062f7665fb38c5220d5e3fadf0cae9b
charmant27 <charmantogatera@gmail.com>
2022-12-17 14:46:37 -0800

    jwt strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,17 +3,40 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
 
-  validate(payload: any) {
-    return payload;
+  async validate(payload: {
+    sub: number;
+    adminID: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+  }) {
+    const admin = await this.prisma.admin.findMany({
+      where: {
+        id: payload.sub,
+        adminID: payload.adminID,
+        email: payload.email,
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+      },
+      select: {
+        id: true,
+        adminID: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+    return admin;
   }
 }
