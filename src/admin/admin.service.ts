75c2d4c80eceebcab0a555a3c773281eb9dce5f9
charmant27 <charmantogatera@gmail.com>
2022-12-21 12:14:08 -0800

    added admin routes

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,6 +1,34 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditAdmin } from './dto';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private prisma: PrismaService) {}
+
+  async editAdmin(adminId: number, dto: EditAdmin) {
+    const admin = await this.prisma.admin.update({
+      where: {
+        id: adminId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete admin.hash;
+    console.log(admin);
+
+    return admin;
+  }
+
+  async deleteAdmin(adminId: number) {
+    const admin = await this.prisma.admin.delete({
+      where: {
+        id: adminId,
+      },
+    });
+
+    return 'admin data was successfully deleted';
+  }
+}
